6e01c898706b02168b3b1360e8ea27a5ba341c67
Raul Perez <raul272@gmail.com>
2021-12-06 17:09:25 +0100

    Day 5

--- day3/day3.js
+++ day3/day3.js
@@ -67,59 +67,5 @@ async function calculate(){
         console.log(parseInt(solRating[0], 2) * parseInt(solScrubber[0], 2));
     });
 }
-//         data.split('\r\n').forEach(line => {
-//             var cols = line.split('');
-//             // Tolerate empty lines. There may be one at the end of the input.
-//             for( let i= 0; i < cols.length; i += 1){
-                
-
-//                 if(!arr[i]){
-//                     arr[i] = {
-//                         one: 0,
-//                         zero: 0  
-//                     };
-//                 }
-
-//                 if(cols[i] === '1'){
-
-//                     arr[i] = {
-//                         one: arr[i].one + 1,
-//                         zero: arr[i].zero  
-//                     };
-//                 }else if(cols[i] === '0'){
-//                     arr[i] = {
-//                         one: arr[i].one,
-//                         zero: arr[i].zero + 1  
-//                     };
-//                 }
-
-//             }
-//         });
-//         console.log(arr);
-//         let numberMost = '', numberLess = '';
-
-
-//         for (const [key, value] of Object.entries(arr)) {
-//             if(value.one > value.zero)
-//             {
-//                 numberMost += '1'
-//                 numberLess += '0'
-//             }else if(value.one < value.zero){
-//                 numberMost += '0'
-//                 numberLess += '1'
-//             }
-//           }
-
-//         numberMost = parseInt(numberMost, 2);
-//         numberLess = parseInt(numberLess, 2);
-//         console.log(numberMost * numberLess);
-
-
-//     });
-    
-
-
-
-// }
 
 calculate();
--- day5/day5.js
+++ day5/day5.js
@@ -1,22 +1,51 @@
-const { readFile } = require('fs');
+const { readFile, readFileSync } = require('fs');
 
 
 async function calculate(){
 
-    const input = await readFile('./day5/input.txt', 'utf-8', (error, data) => {
-
-        const input = data.split('\r\n');
-
-        const coordinates = input.map((value, index, array) => {
-            const values = value.split(' -> ');
-            return values.map(x => {
-               return x.split(',').map(y => parseInt(y));
-            });
+    const input = readFileSync('./day5/input.txt', 'utf-8').split('\n');
+    const coordinates = input.map(value => {
+        const values = value.split(' -> ');
+        return values.map(x => {
+            return x.split(',').map(y => parseInt(y));
         });
-
-        
-
     });
+    
+
+    const lines = {};
+    const columns = {};
+    const board = new Array(1000).fill(0).map(_ => new Array(1000).fill(0));
+
+    coordinates.forEach(coord => {
+        if(coord[0][0] == coord[1][0]){
+            let max = Math.max(coord[0][1], coord[1][1]);
+            let min = Math.min(coord[0][1], coord[1][1]);
+            for(let i = min; i <= max; i++){
+                board[coord[0][0]][i] += 1;
+            }
+           
+        }
+        if(coord[0][1] == coord[1][1]){
+            // console.log('Line ' + coord[0][1]);
+            
+            let max = Math.max(coord[0][0], coord[1][0]);
+            let min = Math.min(coord[0][0], coord[1][0]);
+            for(let i = min; i <= max; i++){
+                board[i][coord[0][1]] += 1;
+            }
+        }
+    });
+
+    let number = 0;
+    const a = board.forEach( x =>  {
+        number += x.filter(y => y == 2).length;
+    }
+    );
+
+    console.log(number);
+    console.log(a);
+
+
 }
 
 
